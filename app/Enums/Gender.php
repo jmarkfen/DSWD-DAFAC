80ae8bd27abc2376f61d4fd98b43daf504768306
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2024-05-07 14:20:14 +0800

    Improve labels and input type

--- app/Enums/Gender.php
+++ app/Enums/Gender.php
@@ -2,8 +2,16 @@
 
 namespace App\Enums;
 
-enum Gender: string
+enum Gender: string implements \Filament\Support\Contracts\HasLabel
 {
     case MALE = 'Male';
     case FEMALE = 'Female';
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::MALE => 'Male',
+            self::FEMALE => 'Female',
+        };
+    }
 }
