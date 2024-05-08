a4a521422f2bc9e081c9ce23acf262656c2987d6
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2024-05-08 10:02:09 +0800

    Fix display lettercase for ID Card Type

--- app/Filament/Resources/IdCardTypeResource.php
+++ app/Filament/Resources/IdCardTypeResource.php
@@ -19,6 +19,8 @@ class IdCardTypeResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $modelLabel = 'ID Card Type';
+
     public static function form(Form $form): Form
     {
         return $form
