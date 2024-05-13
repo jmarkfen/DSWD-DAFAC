072752d3f230b4e7fb66ea8d9f44d8282d7893a9
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2024-05-13 15:27:06 +0800

    Hook session variables into form

--- app/Filament/Resources/DafacResource/Pages/CreateDafac.php
+++ app/Filament/Resources/DafacResource/Pages/CreateDafac.php
@@ -9,4 +9,15 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateDafac extends CreateRecord
 {
     protected static string $resource = DafacResource::class;
+
+    protected function afterFill(): void
+    {
+        $this->data['first_name'] = session('first_name');
+        $this->data['last_name'] = session('last_name');
+        $this->data['middle_name'] = session('middle_name');
+        $this->data['name_extension'] = session('name_extension');
+        $this->data['birthdate'] = session('birthdate');
+        $this->data['birthplace'] = session('birthplace');
+        $this->data['sex'] = session('sex');
+    }
 }
